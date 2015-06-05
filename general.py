5620b8e5568bb3df2ba63d7fa209ec6f72a439b6
David Manouchehri <david@davidmanouchehri.com>
2015-06-04 23:25:29 -0400

    Cleaned the headers. Able to get the time with no issues.

--- general.py
+++ general.py
@@ -2,6 +2,7 @@ import urllib.request
 import gzip
 import json
 
+# These default headers are not used.
 headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Accept-Encoding': 'gzip, deflate',
            'Accept-Language': 'en-us',
@@ -15,9 +16,9 @@ API_ver = "v1/"
 
 # https://practicelogin.questrade.com/Signin.aspx?ReturnUrl=%2fAPIAccess%2fUserApps.aspx
 
-def grab(url, headers=headers):
+
+def grab(url):
     req = urllib.request.Request(url, headers=headers)
-    print(headers)
     try:
         response = urllib.request.urlopen(req)
         if response.info().get('Content-Encoding') == 'gzip':
@@ -28,44 +29,44 @@ def grab(url, headers=headers):
             raise ValueError('Encoding type unknown')
         return json.loads(page_data.decode('utf-8'))
     except urllib.error.HTTPError as err:
-        print("Something went wrong.")
-        print(err.code)
+        print(err.code + " when fetching " + url)
         raise err
 
 
 def get_tokens():
 
-    with open('auth.json') as inf:
+    auth_file = 'auth.json'
+    with open(auth_file) as inf:
         parsed = json.load(inf)
-    printJSON(parsed)
+
+    print("Loaded " + auth_file)
 
     try:
+        global headers
         headers = {"Authorization": parsed['token_type'] + " " + parsed['access_token']}
-        temp_data = grab(parsed['api_server'] + API_ver + "time", headers)
-        print(temp_data)
+        global API_URL
+        API_URL = parsed['api_server'] + API_ver
+        print(grab(API_URL + "time"))
     except:
-        print("oops")
+        print("Expired access token, creating new one...")
         refresh_url = "/oauth2/token?grant_type=refresh_token&refresh_token="
         data_reply = grab(baseHost[0] + refresh_url + parsed['refresh_token'])
         if not data_reply:
+            print("Expired refresh key.")
             print("Go to: " + baseHost[0] + "/Signin.aspx?ReturnUrl=%2fAPIAccess%2fUserApps.aspx")
-            data_reply = grab(baseHost[0] + refresh_url + input("Please enter your refresh key: "))
+            data_reply = grab(baseHost[0] + refresh_url + input("Please enter your new refresh key: "))
 
         if data_reply:
-            with open('auth.json', 'w') as outf:
+            with open(auth_file, 'w') as outf:
                 json.dump(data_reply, outf)
-                parsed = json.load(outf)
+            with open(auth_file) as inf:
+                parsed = json.load(inf)
         else:
             raise ValueError("Failure to get tokens.")
-
-    return {"Authorization": parsed['token_type'] + " " + parsed['access_token']}, parsed['api_server']
 
 def printJSON(dump):
     print(json.dumps(dump, indent=4, sort_keys=True))
 
+get_tokens()
 
-headers, API_server = get_tokens()
-
-API_URL = API_server + API_ver
-
-print(grab(API_URL + "time", headers))  # I want to avoid typing the second headers.
+print(grab(API_URL + "time"))
